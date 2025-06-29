8dde2ad7d9178e7d5e07cfcf7cea92e4ce7a5013
Amirhossein-Mp <amirhossein.mp004@gmail.com>
2025-06-29 17:22:14 +0330

    134. Gas Station Solved!

--- 238.cpp
+++ 238.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
-    vector<int> productExceptSelf(vector<int>& nums) {
-        int size=nums.size();
-        vector<int> prefix(size);
-        vector<int>postfix(size);
-        vector <int> answer(size);
-        prefix[0]=1;
-        postfix[size-1]=1;
-        for(int i=1;i<size;i++)
-            prefix[i]=prefix[i-1]*nums[i-1];
-        for(int i=size-2;i>=0;i--)
-            postfix[i]=postfix[i+1]*nums[i+1];
-        for(int i=0;i<size;i++)
-            answer[i]=prefix[i]*postfix[i];
-        return answer;
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int gasSize=gas.size();
+        int index=0;
+        int sum=0;
+        for (int i=0;i<gasSize;i++)
+            sum+=gas[i]-cost[i];
+        if(sum<0)
+            return -1;
+        sum=0;
+        for (int i=0;i<gasSize;i++){
+            sum+=gas[i]-cost[i];
+            if(sum<0){
+                sum=0;
+                index=i+1;
+            }
+        }
+        return index;
     }
 };
