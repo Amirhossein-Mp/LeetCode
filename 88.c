a97021b5b7f3b0779e3a024adfde13af8e59627c
Amirhossein-Mp <amirhossein.mp004@gmail.com>
2025-06-29 11:12:05 +0330

    Update 88.c solution and remove executable file

--- 88.c
+++ 88.c
@@ -1,32 +1,31 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-int main(){
-    vector<int> nums={1,1,1,2,2,3};
-    int size=nums.size();
-        int last=0;
-        int num=1;
-        int i=1;
-        int k=1;
-        while(i<size){
-            if(nums[i]!=nums[last]){
-                // cout<<"1"<<endl;
-                nums[++last]=nums[i];
-                num=1;
-                i++;
+void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
+    if (m != 0 & n != 0) {
+        int* finalArray = (int*)malloc((m + n) * sizeof(int));
+        int j = 0;
+        int k = 0;
+        for (int i = 0; i < m + n; i++) {
+            if( j<m & k<n){
+                if (nums1[j] < nums2[k] ) {
+                    finalArray[i] = nums1[j];
+                    j++;
+                } else {
+                    finalArray[i] = nums2[k];
+                    k++;
+                }
+            }else if(j<m){
+                finalArray[i]=nums1[j];
+                j++;
+            }else{
+                finalArray[i]=nums2[k];
                 k++;
-            }else if((nums[i]==nums[last]) & (num&1)){
-                // cout<<"2"<<endl;
-                last++;
-                num++;
-                i++;
-                k++;
-            }else if((nums[i]==nums[last]) & !(num&1))
-                i++;
+            }
         }
-        return k;
-    for (int j=0;j<=nums.size();j++)
-        cout<<nums[j]<<",";
-    
-    return 0;
+        for (int i = 0; i < m + n; i++)
+            nums1[i] = finalArray[i];
+        free(finalArray);
+    }
+    if (m == 0 & n != 0) {
+        for (int i = 0; i < n; i++)
+            nums1[i] = nums2[i];
+    }
 }
